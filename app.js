5f3a8698888f323f1c79b3dc8bb3907a934ad356
sanjeet <you@example.com>
2017-12-31 22:23:35 +0530

    description field and show page odf campground  added

--- app.js
+++ app.js
@@ -9,15 +9,17 @@
 
 // SCHEMA SETUP FOR CAMPGROUND
  var campgroundSchema = new mongoose.Schema({
- 	name:String,
- 	image:String
+ 	name: String,
+ 	image: String,
+ 	description: String
  });
 
  var Campground = mongoose.model("Campground", campgroundSchema);
 
  // Campground.create({
  // 	name : "Tsomoriri Camp", 
- // 	image : "https://cdn.kettik.com/covers/176/images/b2fc10318b61fa9170da520b5765fc66medium.jpg"
+ // 	image : "https://cdn.kettik.com/covers/176/images/b2fc10318b61fa9170da520b5765fc66medium.jpg",
+ // 	description : "From the land of the lamas, Thiksey Monastery welcomes the adventurous traveller to our unique destination, the Chamba Camp, Thiksey. The Ultimate Travelling Camp introduces glamping in India "
  // }, function(err,campground) {
  // 	if(err){
  // 		console.log(err);
@@ -32,7 +34,7 @@
  	res.render("landing");
  });
 
-// ALL CAMPGROUNDS PAGE
+// INDEX ROUTE -> TO SHOW ALL CAMPGROUNDS
  app.get('/campgrounds',function (req,res) {
  	// GET ALL CAMPGROUNDS FROM DB
  	Campground.find({},function(err,allCampgrounds) {
@@ -40,16 +42,18 @@
  			console.log(err);
  		}
  		else{
- 			res.render("campgrounds",{camps:allCampgrounds});
+ 			res.render("index",{camps:allCampgrounds});
  		}
  	});
  });
 
+// CREATE ROUTE -> TO CREATE A NEW CAMPGROUND TO DB
  app.post('/campgrounds',function (req,res) {
  	// GET DATA FROM NFORM AND CREATE NEW CAMPGROUND
  	var name = req.body.name;
  	var image = req.body.image;
- 	var newcamp = {name:name , image:image};
+ 	var desc = req.body.description;
+ 	var newcamp = {name:name , image:image, description:desc};
  	// CREATE NEW CAMPGROUND AND SAVE TO DB
  	Campground.create(newcamp,function (err, newlyCreated) {
  		if(err){
@@ -63,10 +67,25 @@
  	
  });
 
+// NEW ROUTE -> TO SHOW FORM TO CREATE A NEW CAMPGROUND
  app.get('/campgrounds/new',function (req,res) {
- 	res.render("new.ejs");
+ 	res.render("new");
  });
 
+// SHOW ROUTE -> TO SHOW DETAILS OF A CAMPGROUND
+ app.get('/campgrounds/:id',function (req,res) {
+ 	// FIND CAMPGROUND WITH PROVIDED ID
+ 	Campground.findById(req.params.id,function (err,foundCampground) {
+ 		if(err){
+ 			console.log(err);
+ 		}
+ 		else{
+ 			// RENDER SHOW TEMPLATE OF THAT CAMPGROUND
+ 			res.render("show", {campground : foundCampground});
+ 		}
+ 	});
+ 	
+ });
 
 
  app.listen(3000,function() {
